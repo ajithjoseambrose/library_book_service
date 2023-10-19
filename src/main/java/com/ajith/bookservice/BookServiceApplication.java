af3dae0d94b050595da76a8f1dc5f0389205a537
ajithjoseambrose <ajithj71@gmail.com>
2023-10-19 20:35:40 +0530

    new configuration of namespace endpoints added

--- src/main/java/com/ajith/bookservice/BookServiceApplication.java
+++ src/main/java/com/ajith/bookservice/BookServiceApplication.java
@@ -2,10 +2,8 @@ package com.ajith.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
